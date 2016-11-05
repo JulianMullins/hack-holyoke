b3124201b32430fe6cdcc5aca377774cf7bc4269
Julian Mullins <mullinsjulian@gmail.com>
2016-11-05 00:09:20 -0400

    styles

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var websiteTitle = 'College Rides';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'College Rides' });
+  res.render('index', {title: websiteTitle});
 });
 
 router.get('/signupPass', function(req, res, next) {
-	res.render('signupPass');
+	res.render('signupPass', {title: websiteTitle});
 })
 
 router.get('/signupDriver', function(req, res, next) {
-	res.render('signupDriver');
+	res.render('signupDriver', {title: websiteTitle});
 })
 
 router.get('/login', function(req, res, next) {
-	res.render('login');
+	res.render('login', {title: websiteTitle});
 })
 
 router.get('/postRide', function(req, res, next) {
-	res.render('postRide');
+	res.render('postRide', {title: websiteTitle});
 })
 
 module.exports = router;
