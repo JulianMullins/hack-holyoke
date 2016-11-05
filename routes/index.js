bbed1e0de0a983b4e1582422432992b3c26b91fc
Julian Mullins <mullinsjulian@gmail.com>
2016-11-04 22:52:18 -0400

    nodemon

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'College Rides' });
 });
 
+router.get('/signupPass', function(req, res, next) {
+	res.render('signupPass');
+})
+
+router.get('/signupDriver', function(req, res, next) {
+	res.render('signupDriver');
+})
+
+router.get('/login', function(req, res, next) {
+	res.render('login');
+})
+
+router.get('/postRide', function(req, res, next) {
+	res.render('postRide');
+})
+
 module.exports = router;
